a8c20c0c028093682fee845b52f1ff6016b0b986
Alex Elkins <alexelkins23@gmail.com>
2024-03-02 21:34:16 -0500

    Add functionality to buttons

--- script.js
+++ script.js
@@ -7,14 +7,122 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+  let val;
+  if (b === "" || a === "ERROR" || b === "ERROR") return "ERROR";
+  if (operator === "") return a;
   switch (operator) {
     case "+":
-      return add(a, b);
+      val = add(a, b);
+      break;
     case "-":
-      return subtract(a, b);
+      val = subtract(a, b);
+      break;
     case "*":
-      return multiply(a, b);
+      val = multiply(a, b);
+      break;
     case "/":
-      return divide(a, b);
+      val = divide(a, b);
+      break;
   }
+  if (val > 999999999999 || val < -999999999999) return "ERROR";
+  return val;
 }
+
+let result = document.getElementById("result");
+let a = "";
+let b = "";
+let operator = "";
+let operatorBool = false;
+let reset = false;
+
+const operators = document.getElementsByClassName("op");
+const opArray = [...operators];
+opArray.forEach((element) => {
+  element.addEventListener("click", function() {
+    if (element.textContent === "=") {
+      b = result.textContent;
+      result.textContent = operate(a, b, operator).toString().slice(0, 13);
+      a = result.textContent;
+      b = "";
+      operator = "";
+      operatorBool = false;
+      reset = true;
+    } else {
+      if (operatorBool) {
+        b = result.textContent;
+        result.textContent = operate(a, b, operator).toString().slice(0, 13);
+        a = result.textContent;
+        b = "";
+      }
+      a = result.textContent;
+      operator = element.textContent;
+      operatorBool = true;
+      reset = true;
+    }
+  })
+})
+
+const btns = document.getElementsByClassName("btn");
+const btnArray = [...btns];
+btnArray.forEach((element) => {
+  element.addEventListener("click", function() {
+    let max;
+    if (result.textContent.includes("-")) max = 13;
+    else max = 12;
+    if (result.textContent.length < max || reset) {
+      if (result.textContent === "ERROR" || result.textContent === "0" || reset) {
+        result.textContent = element.textContent;
+        reset = false;
+      } else {
+        result.textContent += element.textContent;
+      }
+    }
+  })
+})
+
+const zero = document.getElementById("zero");
+zero.addEventListener("click", function() {
+  let max;
+  if (result.textContent.includes("-")) max = 13;
+  else max = 12;
+  if (result.textContent.length < max || reset) {
+    if (result.textContent === "ERROR" || reset) result.textContent = "0";
+    if (result.textContent != "0") result.textContent += "0";
+  }
+})
+
+const dot = document.getElementById("dot");
+dot.addEventListener("click", function() {
+  if (result.textContent === "ERROR" || reset) {
+    result.textContent = "0.";
+    reset = false;
+  }
+  if (!(result.textContent.includes("."))) {
+    result.textContent += ".";
+    reset = false;
+  }
+})
+
+const clr = document.getElementById("AC");
+clr.addEventListener("click", function() {
+  operator = "";
+  a = "";
+  b = "";
+  operatorBool = false;
+  reset = false;
+  result.textContent = "0";
+})
+
+const pm = document.getElementById("pm");
+pm.addEventListener("click", function() {
+  if (!(result.textContent === "ERROR")) {
+    result.textContent = (+result.textContent * -1).toString();
+  }
+})
+
+const percent = document.getElementById("%");
+percent.addEventListener("click", function() {
+  if (!(result.textContent === "ERROR")) {
+    result.textContent = (+result.textContent / 100).toString();
+  }
+})
